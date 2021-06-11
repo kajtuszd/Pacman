f012b667dafae1b0cd3ac081b64f181ca8aa4cfc
adamek195 <248955@student.pwr.edu.pl>
2021-06-11 15:33:05 +0200

    clean up

--- src/pacman/Ghost.java
+++ src/pacman/Ghost.java
@@ -82,4 +82,35 @@ abstract class Ghost extends Character {
             turnDown();
         }
     }
+
+    protected void evaluateNextMove(short[] mazeData) {
+        if (mazeData[countNextLocation(moveDX, moveDY)] == 0 || mazeData[countNextLocation(moveDX, moveDY)] == 0
+                || mazeData[countNextLocation(moveDX, moveDY)] == 4) {
+            setMoveVector(0,0);
+        }
+        if (isOnNode(mazeData) || (moveDX == 0 && moveDY == 0)) {
+            int randomNumber = (int) ((Math.random() * (4)));
+            int vectX = 0, vectY = 0;
+            if (randomNumber == 0 && mazeData[countNextLocation(-1, 0)] != 0) { // left
+                vectX = -1;
+                vectY = 0;
+            }
+            if (randomNumber == 1 && mazeData[countNextLocation(1, 0) + 1] != 0) { // right
+                vectX = 1;
+                vectY = 0;
+            }
+            if (randomNumber == 2 && mazeData[countNextLocation(0, 1) + 28] != 0 &&
+                    mazeData[countNextLocation(0, 1) + 28] != 4) { // down
+                vectX = 0;
+                vectY = 1;
+            }
+            if (randomNumber == 3 && mazeData[countNextLocation(0, -1 )] != 0) { // up
+                vectX = 0;
+                vectY = -1;
+            }
+            decideWhereToMove(vectX, vectY);
+        }
+        updateCoordinates();
+        goThroughTunnelAndChangeSide();
+    }
 }
